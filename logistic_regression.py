0bfce44c65f5485ac90bda05eea6a493cedda5f0
Sergio Hernandez-Acosta <79073281+sergiojhernandezacosta@users.noreply.github.com>
2023-02-16 15:22:08 -0600

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -12,13 +12,26 @@ y = (X[:, 0] + X[:, 1] > 0).astype(np.int)
 plt.scatter(X[:, 0], X[:, 1], c=y)
 plt.xlabel('Feature 1')
 plt.ylabel('Feature 2')
-plt.show()
 
 # Fit a logistic regression model to the data
 model = LogisticRegression()
 model.fit(X, y)
 
-# Predict the class of a new data point
-new_point = np.array([[0.5, 0.5]])
-predicted_class = model.predict(new_point)
-print("Predicted class:", predicted_class)
+# Get the coefficients of the logistic regression model
+coef = model.coef_[0]
+intercept = model.intercept_
+
+# Compute the slope and intercept of the line that separates the two classes
+x1 = X[:, 0].min()
+x2 = X[:, 0].max()
+y1 = -(intercept + coef[0] * x1) / coef[1]
+y2 = -(intercept + coef[0] * x2) / coef[1]
+
+# Add the dividing line to the plot
+plt.plot([x1, x2], [y1, y2], color='black')
+
+plt.show()
+
+slope = -(intercept / coef[1])
+intercept = -(coef[0] / coef[1])
+print(f"Equation of line:  y = {slope}x{intercept}")
